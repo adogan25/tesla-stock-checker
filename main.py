bf47b4cf11ab411ec0090e3d51149b99579f1240
adogan25 <doganalpcs@hotmail.com>
2025-05-05 09:43:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def check_for_rear_wheel_drive():
                 return True
     except Exception as e:
         print(f"Hata oluştu: {e}")
-    return False
+    return True
 
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
